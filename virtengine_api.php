88e91f8f706e21963031483ceeaf2ac24d64e684
Rajesh-Rajagopal <rajram.rajesh21@gmail.com>
2016-12-19 22:28:04 +0530

    Whmcs hooks fix

--- virtengine_api.php
+++ virtengine_api.php
@@ -1,17 +1,14 @@
 include(ROOTDIR.'/includes/hooks/virtengine_db.php');
-
 <?php
-
 //====== TO_DO: START: PLEASE CUSTOMIZE THIS AS PER YOUR SITE.
 define(MASTER_KEY, '3b8eb672aa7c8db82e5d34a0744740b20ed59e1f6814cfb63364040b0994ee3f');
-define(GATEWAY, '13.70.94.139:9000');
+define(GATEWAY, '146.0.247.2:9000');
 //====== TO_DO: END: PLEASE CUSTOMIZE THIS AS PER YOUR SITE.
-
 function build_hmac($api_url, $data, $user_id) {
 //Converting the body into md5 hash
   $body_digest = openssl_digest( $data,'md5', true );
   //Encoding the body_digest with base64 encde
-  $encoded_body = base64_encode( $body_digest );
+  $encoded_body = str_replace(array('+', '/'), array('-', '_'), base64_encode($body_digest));
   //Current date for example
   $current_date = date('Y-m-d H:i');
   //Forming the signature
@@ -19,34 +16,25 @@ function build_hmac($api_url, $data, $user_id) {
   //Get Client Custom Fields
   $vertice_apikey = (MASTER_KEY);
   $vertice_email = fetch_user($user_id);
-
   logActivity("=Debug: ---  build_hmac ".$api_url);
-  logActivity("=Debug: sign   is".$signature);
-  logActivity("=Debug: email  is".$vertice_email);
-  logActivity("=Debug: apikey is".$vertice_apikey);
 
   //Creating HMAC hash with sha256
   $hash = hash_hmac( 'sha256', rtrim($signature), $vertice_apikey );
   //Final Hmac
   $final_hmac = $vertice_email . ':' . $hash;
-
   //we are sticking it in a hash
   $built=array();
   $built['final_hmac']=$final_hmac;
   $built['current_date']=$current_date;
-
   return $built;
 }
-
 function build_header($headerArgs, $user_id) {
   $final_hmac = $headerArgs['final_hmac'];
   $current_date = $headerArgs['current_date'];
   $organization_id = fetchFieldByName('org_id',$user_id);
-
   logActivity("=Debug: ---  build_header");
   logActivity("=Debug: final_hmac:".$final_hmac);
   logActivity("=Debug: currrent_date:".$current_date);
-
   $headers =  array(
     'Accept: application/json',
     'Content-Type: application/json',
@@ -56,23 +44,17 @@ function build_header($headerArgs, $user_id) {
     'X-Megam-HMAC: '.$final_hmac,
     'X-Megam-ORG: '.$organization_id,
     'X-Megam-MASTERKEY: true');
-
     return $headers;
 }
 function invoke_api($api_url, $body_json, $user_id) {
   logActivity("=Debug: ---  Vertice API: STARTS");
-  logActivity("=Debug: ---  API Parms:".$body_json);
+  logActivity("=Debug: ---  API Parms:".json_encode($body_json));
   logActivity("=Debug: ---  API Userid:".$user_id);
-
   $data = json_encode($body_json);
-
   $headerArgs = build_hmac($api_url,$data, $user_id);
-
   $headers = build_header($headerArgs, $user_id);
-
   $ch = curl_init();
   curl_setopt($ch, CURLOPT_URL, (GATEWAY).$api_url);
-
   curl_setopt($ch,CURLOPT_HTTPHEADER, $headers);
   curl_setopt($ch, CURLOPT_POST, true);
   curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
@@ -83,9 +65,7 @@ function invoke_api($api_url, $body_json, $user_id) {
   $response = curl_exec($ch);
   $get_info = curl_getinfo($ch, CURLINFO_HTTP_CODE);
   $curl_error = curl_error ( $ch );
-
   curl_close($ch);
   return  array('Request: ' => $body_json, 'Api Response : ' => $response,'Http Code : ' => $get_info,'Curl Error : ' => $curl_error);
 }
-
 ?>
