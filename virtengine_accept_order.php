d65da856f89498cd001b320cd26d29cc81f5d053
Kishorekumar Neelamegam <nkishore@megam.co.in>
2016-12-19 12:32:13 +0530

    Accept order, get the clietns products

--- virtengine_accept_order.php
+++ virtengine_accept_order.php
@@ -47,6 +47,70 @@ function after_accept_order($vars) {
     */
 }
 
+function getClientProducts() {
+$result = select_query("tblhosting", "COUNT(*)", $where, "", "", "", "tblproducts ON tblproducts.id=tblhosting.packageid INNER JOIN tblproductgroups ON tblproductgroups.id=tblproducts.gid");
+$data = mysql_fetch_array($result);
+$totalresults = $data[0];
+$limitstart = (int)$limitstart;
+$limitnum = (int)$limitnum;
+
+if (!$limitnum) {
+	$limitnum = 999999;
+}
+
+$result = select_query("tblhosting", "tblhosting.*,tblproducts.name AS productname,tblproductgroups.name AS groupname,(SELECT CONCAT(name,'|',ipaddress,'|',hostname) FROM tblservers WHERE tblservers.id=tblhosting.server) AS serverdetails,(SELECT tblpaymentgateways.value FROM tblpaymentgateways WHERE tblpaymentgateways.gateway=tblhosting.paymentmethod AND tblpaymentgateways.setting='name' LIMIT 1) AS paymentmethodname", $where, "tblhosting`.`id", "ASC", "" . $limitstart . "," . $limitnum, "tblproducts ON tblproducts.id=tblhosting.packageid INNER JOIN tblproductgroups ON tblproductgroups.id=tblproducts.gid");
+$apiresults = array("result" => "success", "clientid" => $clientid, "serviceid" => $serviceid, "pid" => $pid, "domain" => $domain, "totalresults" => $totalresults, "startnumber" => $limitstart, "numreturned" => mysql_num_rows($result));
+
+if (!$totalresults) {
+	$apiresults['products'] = "";
+}
+
+
+
+while ($data = mysql_fetch_array($result)) {
+	$id = $data['id'];
+	$userid = $data['userid'];
+	$orderid = $data['orderid'];
+	$pid = $data['packageid'];
+	$name = $data['productname'];
+	$groupname = $data['groupname'];
+	$server = $data['server'];
+	$regdate = $data['regdate'];
+	$domain = $data['domain'];
+	$paymentmethod = $data['paymentmethod'];
+	$paymentmethodname = $data['paymentmethodname'];
+	$firstpaymentamount = $data['firstpaymentamount'];
+	$recurringamount = $data['amount'];
+	$billingcycle = $data['billingcycle'];
+	$nextduedate = $data['nextduedate'];
+	$domainstatus = $data['domainstatus'];
+	$username = $data['username'];
+	$password = decrypt($data['password']);
+	$notes = $data['notes'];
+	$subscriptionid = $data['subscriptionid'];
+	$promoid = $data['promoid'];
+	$ipaddress = $data['ipaddress'];
+	$overideautosuspend = $data['overideautosuspend'];
+	$overidesuspenduntil = $data['overidesuspenduntil'];
+	$ns1 = $data['ns1'];
+	$ns2 = $data['ns2'];
+	$dedicatedip = $data['dedicatedip'];
+	$assignedips = $data['assignedips'];
+	$diskusage = $data['diskusage'];
+	$disklimit = $data['disklimit'];
+	$bwusage = $data['bwusage'];
+	$bwlimit = $data['bwlimit'];
+	$lastupdate = $data['lastupdate'];
+	$serverdetails = $data['serverdetails'];
+	$serverdetails = explode("|", $serverdetails);
+	$customfieldsdata = array();
+	$customfields = getCustomFields("product", $pid, $id, "on", "");
+	foreach ($customfields as $customfield) {
+		$customfieldsdata[] = array("id" => $customfield['id'], "name" => $customfield['name'], "value" => $customfield['value']);
+	}
+  }
+}
+
 add_hook("AcceptOrder",1,"after_accept_order");
 
 class Quotas {
